f66575023efae8aa77fbf969dd4ee1ab2f43a2a1
Adam Roman <adam.roman@bergenlogistics.com>
2020-03-27 15:25:13 -0400

    First Round of Changes

--- app.py
+++ app.py
@@ -29,11 +29,11 @@ def format_response(event):
         text = "OK... Calculating Funniest Joke Ever Created..."
     else:
         text = ""
-    return {'text':text}
+    return {'text': text}
 
 @app.route('/', methods=['GET'])
 def home_get():
     return render_template("home.html")
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run(port=8080, debug=True)
